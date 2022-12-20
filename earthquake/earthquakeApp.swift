6eeffb03c81ca9996d48230d0b4d63293ccebdce
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2022-12-20 19:41:15 +0000

    Initial Commit

---
+++ earthquake/earthquakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  earthquakeApp.swift
+//  earthquake
+//
+//  Created by Kaylee Williams on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct earthquakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
